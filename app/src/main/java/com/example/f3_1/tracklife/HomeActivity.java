3ce7db0ef465d3f439c4aa2cbb48d4ad33b22b51
valkeshpatel123@gmail.com <valkeshpatel123@gmail.com>
2017-07-03 19:48:06 +0530

    Formate code

--- app/src/main/java/com/example/f3_1/tracklife/HomeActivity.java
+++ app/src/main/java/com/example/f3_1/tracklife/HomeActivity.java
@@ -16,12 +16,11 @@ import java.io.File;
  */
 
 public class HomeActivity extends Activity {
-    Button btnSignIn,btnSignUp;
+    Button btnSignIn, btnSignUp;
     LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         String folder_main = "SQLiteDataBase";
@@ -34,12 +33,12 @@ public class HomeActivity extends Activity {
 
         // create a instance of SQLite Database
         Object context = null;
-        loginDataBaseAdapter=new LoginDataBaseAdapter(this, HomeActivity.this);
-        loginDataBaseAdapter=loginDataBaseAdapter.open();
+        loginDataBaseAdapter = new LoginDataBaseAdapter(this, HomeActivity.this);
+        loginDataBaseAdapter = loginDataBaseAdapter.open();
 
         // Get The Refference Of Buttons
-        btnSignIn=(Button)findViewById(R.id.buttonSignIN);
-        btnSignUp=(Button)findViewById(R.id.buttonSignUP);
+        btnSignIn = (Button) findViewById(R.id.buttonSignIN);
+        btnSignUp = (Button) findViewById(R.id.buttonSignUP);
 
         // Set OnClick Listener on SignUp button
         btnSignUp.setOnClickListener(new View.OnClickListener() {
@@ -47,47 +46,45 @@ public class HomeActivity extends Activity {
                 // TODO Auto-generated method stub
 
                 /// Create Intent for SignUpActivity  abd Start The Activity
-                Intent intentSignUP=new Intent(HomeActivity.this,SignUPActivity.class);
+                Intent intentSignUP = new Intent(HomeActivity.this, SignUPActivity.class);
                 startActivity(intentSignUP);
             }
         });
     }
+
     Dialog dialog;
+
     // Methos to handleClick Event of Sign In Button
-    public void signIn(View V)
-    {
+    public void signIn(View V) {
         dialog = new Dialog(HomeActivity.this);
         dialog.setContentView(R.layout.login);
         dialog.setTitle("Login");
 
         // get the Refferences of views
-        final EditText editTextUserName=(EditText)dialog.findViewById(R.id.editTextUserNameToLogin);
-        final  EditText editTextPassword=(EditText)dialog.findViewById(R.id.editTextPasswordToLogin);
+        final EditText editTextUserName = (EditText) dialog.findViewById(R.id.editTextUserNameToLogin);
+        final EditText editTextPassword = (EditText) dialog.findViewById(R.id.editTextPasswordToLogin);
 
-        Button btnSignIn = (Button)dialog.findViewById(R.id.buttonSignIn);
+        Button btnSignIn = (Button) dialog.findViewById(R.id.buttonSignIn);
 
         // Set On ClickListener
         btnSignIn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
                 // get The User name and Password
-                String userName=editTextUserName.getText().toString();
-                String password=editTextPassword.getText().toString();
+                String userName = editTextUserName.getText().toString();
+                String password = editTextPassword.getText().toString();
 
                 // fetch the Password form database for respective user name
                 String storedPassword = loginDataBaseAdapter.getSinlgeEntry(userName);
 
                 // check if the Stored password matches with  Password entered by user
-                if(password.equals(storedPassword))
-                {
+                if (password.equals(storedPassword)) {
                     // Toast.makeText(HomeActivity.this, "Congrats: Login Successfull", Toast.LENGTH_LONG).show();
                     dialog.dismiss();
-                    Intent i = new Intent(HomeActivity.this,MainActivity.class);
+                    Intent i = new Intent(HomeActivity.this, MainActivity.class);
                     startActivity(i);
 
-                }
-                else
-                {
+                } else {
                     Toast.makeText(HomeActivity.this, "User Name or Password does not match", Toast.LENGTH_LONG).show();
                 }
             }
--- app/src/main/java/com/example/f3_1/tracklife/LoginDataBaseAdapter.java
+++ app/src/main/java/com/example/f3_1/tracklife/LoginDataBaseAdapter.java
@@ -18,8 +18,8 @@ public class LoginDataBaseAdapter {
     public static final int NAME_COLUMN = 1;
     // TODO: Create public field for each column in your table.
     // SQL Statement to create a new database.
-    static final String DATABASE_CREATE = "create table "+"LOGIN"+
-            "( " +"ID"+" integer primary key autoincrement,"+ "USERNAME  text,PASSWORD text); ";
+    static final String DATABASE_CREATE = "create table " + "LOGIN" +
+            "( " + "ID" + " integer primary key autoincrement," + "USERNAME  text,PASSWORD text); ";
     // Variable to hold the database instance
     public SQLiteDatabase db;
     // Context of the application using the database.
@@ -31,64 +31,65 @@ public class LoginDataBaseAdapter {
     public LoginDataBaseAdapter(Context _context, Object context) {
 
         this.context = _context;
-        dbHelper = new DataBaseHelper(this.context, this.context.getExternalCacheDir()+DATABASE_NAME, null, DATABASE_VERSION);
+        dbHelper = new DataBaseHelper(this.context, this.context.getExternalCacheDir() + DATABASE_NAME, null, DATABASE_VERSION);
     }
-    LoginDataBaseAdapter open()throws SQLException {
+
+    LoginDataBaseAdapter open() throws SQLException {
         db = dbHelper.getWritableDatabase();
         return this;
     }
 
-    public void insertEntry(String userName, String password)
-        {
-            ContentValues newValues = new ContentValues();
-            // Assign values for each row.
-            newValues.put("USERNAME", userName);
-            newValues.put("PASSWORD",password);
+    public void insertEntry(String userName, String password) {
+        ContentValues newValues = new ContentValues();
+        // Assign values for each row.
+        newValues.put("USERNAME", userName);
+        newValues.put("PASSWORD", password);
 
-            // Insert the row into your table
-            db.insert("LOGIN", null, newValues);
-            ///Toast.makeText(context, "Reminder Is Successfully Saved", Toast.LENGTH_LONG).show();
-        }
+        // Insert the row into your table
+        db.insert("LOGIN", null, newValues);
+        ///Toast.makeText(context, "Reminder Is Successfully Saved", Toast.LENGTH_LONG).show();
+    }
 
-    public int deleteEntry(String UserName)
-    {
+    public int deleteEntry(String UserName) {
         //String id=String.valueOf(ID);
-        String where="USERNAME=?";
-        int numberOFEntriesDeleted= db.delete("LOGIN", where, new String[]{UserName}) ;
+        String where = "USERNAME=?";
+        int numberOFEntriesDeleted = db.delete("LOGIN", where, new String[]{UserName});
         // Toast.makeText(context, "Number fo Entry Deleted Successfully : "+numberOFEntriesDeleted, Toast.LENGTH_LONG).show();
         return numberOFEntriesDeleted;
     }
 
-    public void close() {db.close();}
-    public  SQLiteDatabase getDatabaseInstance()
-    {
+    public void close() {
+        db.close();
+    }
+
+    public SQLiteDatabase getDatabaseInstance() {
         return db;
     }
 
     public String getSinlgeEntry(String userName) {
         {
-            Cursor cursor=db.query("LOGIN", null, " USERNAME=?", new String[]{userName}, null, null, null);
-            if(cursor.getCount()<1) // UserName Not Exist
+            Cursor cursor = db.query("LOGIN", null, " USERNAME=?", new String[]{userName}, null, null, null);
+            if (cursor.getCount() < 1) // UserName Not Exist
             {
                 cursor.close();
                 return "NOT EXIST";
             }
             cursor.moveToFirst();
-            String password= cursor.getString(cursor.getColumnIndex("PASSWORD"));
+            String password = cursor.getString(cursor.getColumnIndex("PASSWORD"));
             cursor.close();
             return password;
         }
     }
-    public void  updateEntry(String userName,String password)
-    {
+
+    public void updateEntry(String userName, String password) {
         // Define the updated row content.
         ContentValues updatedValues = new ContentValues();
         // Assign values for each row.
         updatedValues.put("USERNAME", userName);
-        updatedValues.put("PASSWORD",password);
+        updatedValues.put("PASSWORD", password);
 
-        String where="USERNAME = ?";
-        db.update("LOGIN",updatedValues, where, new String[]{userName});
+        String where = "USERNAME = ?";
+        db.update("LOGIN", updatedValues, where, new String[]{userName});
     }
 }
 
